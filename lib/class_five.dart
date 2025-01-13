c248f335fdfb32a82e0668cdcd603d638e5289d8
chrisokoriedev <okoriec01@gmail.com>
2025-01-13 08:26:40 +0100

    `Added carousel slider and staggered grid view to class_five.dart and updated pubspec.yaml and pubspec.lock files to include flutter_staggered_grid_view dependency.`

--- lib/class_five.dart
+++ lib/class_five.dart
@@ -9,11 +9,35 @@ class CarouselSliderClass extends StatelessWidget {
     return Scaffold(
         body: ListView(
       children: [
+        const SizedBox(height: 50),
+        const Text('Carosel Slider', style: TextStyle(fontSize: 30)),
+        const SizedBox(height: 10),
         CarouselSlider(
-          items: [],
-          options: CarouselOptions(),
+          items: itemContent,
+          options: CarouselOptions(
+            height: 150,
+            autoPlay: true,
+            viewportFraction: 0.6,
+            enlargeCenterPage: true,
+            autoPlayAnimationDuration: const Duration(milliseconds: 300),
+          ),
         ),
+        const SizedBox(height: 50),
+        const Text(
+          'flutter_staggered_grid_view',
+          style: TextStyle(fontSize: 30),
+        ),
+        
       ],
     ));
   }
 }
+
+List<Widget> itemContent = [
+  Container(width: 300, color: Colors.red),
+  Container(width: 300, color: Colors.black),
+  Container(width: 300, color: Colors.blue),
+  Container(width: 300, color: Colors.brown),
+  Container(width: 300, color: Colors.pink),
+  Container(width: 300, color: Colors.yellow),
+];
